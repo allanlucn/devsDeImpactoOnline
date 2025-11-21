3c8eb49a930b1f4ccb8da0366e3e526cef47f803
step4hnn <laurostephan@gmail.com>
2025-11-21 12:51:41 -0300

    feat: backend skeleton

--- back-end/main.py
+++ back-end/main.py
@@ -1,6 +1,12 @@
 from typing import Union
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from fastapi import FastAPI
+from db.config import engine, get_db
+from schemas import Base
+from schemas.user import User
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -13,3 +19,18 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/usuarios/")
+def criar_usuario(nome: str, email: str, senha: str, db: Session = Depends(get_db)):
+    novo_usuario = User(nome=nome, email=email, senha=senha)
+    db.add(novo_usuario)
+    db.commit()
+    db.refresh(novo_usuario)
+    return {"id": novo_usuario.id, "nome": novo_usuario.nome, "email": novo_usuario.email}
+
+
+@app.get("/usuarios/")
+def listar_usuarios(db: Session = Depends(get_db)):
+    usuarios = db.query(User).all()
+    return usuarios
