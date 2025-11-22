59060927a16d43a2f40c4f8ced870adb93d3791d
Uilames <uilamesa.ferreira@gmail.com>
2025-11-21 22:16:11 -0300

    chore: update init_db function to check database tables and remove drop_all call

--- back-end/init_db.py
+++ back-end/init_db.py
@@ -2,12 +2,13 @@ from db.config import engine
 from schemas.base import Base
 from schemas.user import User
 from schemas.address import Address
+from schemas.documentos import DocumentoGov 
 
 def init_db():
-    print("Creating database tables...")
-    Base.metadata.drop_all(bind=engine)
+    print("Checking database tables...")
+    # Não é necessário fazer isso na nuvem: Base.metadata.drop_all(bind=engine) 
     Base.metadata.create_all(bind=engine)
-    print("Tables created successfully!")
+    print("Tables check completed!")
 
 if __name__ == "__main__":
     init_db()
